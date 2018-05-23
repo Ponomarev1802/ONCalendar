e63208ad00c61c4a834996246b57904a2e3f453a
Ponomarev1802 <Ponomarev1802@mail.ru>
2018-05-23 17:39:25 +0300

    настройка прокси

--- telegram.py
+++ telegram.py
@@ -1,17 +1,17 @@
 import requests
 import telepot
-import urllib3
+from urllib3.contrib.socks import SOCKSProxyManager
 
 
 
 def send_Message(text):
-    proxy_url = "http://static.163.141.4.46.clients.your-server.de:3128"
+    proxy_url = "socks5://s5.open44fz.ru:20180"
 
     telepot.api._pools = {
-        'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
+        'default': SOCKSProxyManager(proxy_url=proxy_url, username="advantage", password="Task!Work", num_pools=3, maxsize=10, retries=False, timeout=30),
     }
     telepot.api._onetime_pool_spec = (
-    urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+    SOCKSProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
     token = "579862720:AAGjj9zdAuneASuenwo7eFLJN6JkKljMgSM"
     TelegramBot = telepot.Bot(token)
     TelegramBot.sendMessage("165958974", text)
